823ccf0740f628692f524374c1b53843bf7dd09b
Usouff <nayaya95.30@gmail.com>
2026-04-17 22:01:15 +0100

    fix: netlify ESM/CJS compatibility

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,10 +1,24 @@
 const serverless = require("serverless-http");
+const express    = require("express");
 
 let handler;
 
 module.exports.handler = async (event, context) => {
   if (!handler) {
-    const { default: app } = await import("../../app.js");
+    // Dynamically import ESM modules at runtime
+    const { profilesRouter } = await import("../../routes/profiles.js");
+
+    const app = express();
+    app.use(express.json());
+    app.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+      res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+      next();
+    });
+    app.options("*", (req, res) => res.sendStatus(204));
+    app.use("/api/profiles", profilesRouter);
+
     handler = serverless(app);
   }
   return handler(event, context);
